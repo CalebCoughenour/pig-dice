2e07a523b1b20a002a521d16a8745d08b1b31937
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-07 09:19:21 -0700

    add gifs for player 1 and player 2
    co-authored-by: Brady Diamond <brdydmnd@gmail.com>
    Co-authored-by: Jeremy Martin <jeremyvs1945@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -43,6 +43,8 @@ let player2 = new Player(false);
 $("#roll").on("click", "#roll-button", function() {    
   player1.diceRoll();
   player2.diceRoll();
+  $("#end").show();
+  $("#kenblock").hide();
   $("#player1-current-score").text("Current Rolls " + player1.currentPlayersScore);
   $("#player2-current-score").text("Current Rolls " + player2.currentPlayersScore);
   if (player1.currentPlayersScore[0] === 1) {
@@ -52,6 +54,14 @@ $("#roll").on("click", "#roll-button", function() {
     $("#player2-current-score").show();
     $("#player1-current-score").hide();
     alert("You rolled a 1 :(")
+    $("#kenblock").show();
+    $('#ryu').hide();
+    $('#ken').hide();
+    $('#bros').hide();
+    $("#ryublock").hide();
+    $("#end").hide();
+    $('#ryu-defense').hide();
+    $('#ken-defense').hide();
   } else if (player2.currentPlayersScore[0] === 1)  {
     player1.turn = true
     player2.turn = false
@@ -59,6 +69,13 @@ $("#roll").on("click", "#roll-button", function() {
     $("#player1-current-score").show();
     $("#player2-current-score").hide();
     alert("You rolled a 1 :(")
+    $("#ryublock").show();
+    $('#ryu').hide();
+    $('#ken').hide();
+    $("#kenblock").hide();
+    $("#end").hide();
+    $('#ryu-defense').hide();
+    $('#ken-defense').hide();
   }
 })
   
@@ -68,11 +85,28 @@ $("#roll").on("click", "#roll-button", function() {
    $("#player1-current-score").hide();
    $("#player2-current-score").hide();
    if (player1.turn === true) {
+    $("#end").hide();
+    $('#ryu').show();
+    $('#ken-defense').show();
+    $('#ken').hide();
+    $('#bros').hide();
+    $("#kenblock").hide();
+    $("#ryublock").hide();
+    $('#ryu-defense').hide();
     $("#player2-current-score").show();
      player1.turn = false;
      player2.turn = true;
    } else { 
-    $("#player1-current-score").show();
+    $("#end").hide();
+    $('#ken').show();
+    $('#ryu-defense').show();
+    $('#ken-defense').hide();
+    $('#ryu').hide();
+    $('#bros').hide();
+    $("#kenblock").hide();
+    $("#ryublock").hide();
+     $("#player1-current-score").show();
+
      player2.turn = false;
      player1.turn = true;
     }
@@ -81,8 +115,21 @@ $("#roll").on("click", "#roll-button", function() {
   
     if (player1.score >= 100) {
       $("#player1-win").show();
+      $('#ryu').hide();
+      $('#bros').hide();
+      $("#kenblock").hide();
+      $("#ryublock").hide();
+      $('#ryu-defense').hide();
+      $('#ken-defense').hide();
     } else if (player2.score >= 100) {
       $("#player2-win").show();
+      $("#player1-win").show();
+      $('#ryu').hide();
+      $('#bros').hide();
+      $("#kenblock").hide();
+      $("#ryublock").hide();
+      $('#ryu-defense').hide();
+      $('#ken-defense').hide();
     }
   })
 
