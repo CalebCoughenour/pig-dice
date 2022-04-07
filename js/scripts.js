8f90c7b6a8b8c7af1ae0c8e889f074e683f015ea
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-07 13:05:11 -0700

    begin adding AI to UI logic in script
    Co-authored-by: Jeremy Martin <jeremyvs1945@gmail.com>
    co-authored-by: Brady Diamond <brdydmnd@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -33,12 +33,57 @@ Player.prototype.updatePlayerScore = function() {
   return this.score
 }
 
+Player.prototype.aiRoll = function()  {
+  let turnTotal = 0
+  while (turnTotal < 20) {
+    turnTotal = 0
+    this.diceRoll();
+    if (this.currentPlayersScore[0] === 1)  {
+      this.currentPlayersScore.splice(0, this.currentPlayersScore.length);
+      turnTotal = 0;
+      return this.score
+    } else  {
+      for (let i = 0; i < this.currentPlayersScore.length; i++) {
+        turnTotal += this.currentPlayersScore[i]
+      }
+    }
+  }
+  this.score += turnTotal
+  this.currentPlayersScore.splice(0, this.currentPlayersScore.length);
+  turnTotal = 0;
+  return this.score
+}
+
 //User Interface Logic
 
 $(document).ready(function() {
 
 let player1 = new Player(true);
 let player2 = new Player(false);
+
+$("#ai-checkbox").click(function() {
+  let checked = document.querySelector("#ai-checkbox:checked") !== null;
+
+  do {
+    console.log(player1.turn)
+    console.log(player2.turn)
+    if (player1.currentPlayersScore[0] === 1) {
+      player1.turn = false
+      player2.turn = true
+    } if (player1.turn === false) {
+      $("#player2-current-score").show();
+      player2.aiRoll();
+      console.log(player2.score);
+        if (player2.currentPlayersScore[0] === 1)  {
+          player1.turn = true
+          player2.turn = false
+          player2.currentPlayersScore.pop();
+        }
+      }
+  } 
+  while (checked === true && player2.turn === true)  
+})
+
 
 $("#roll").on("click", "#roll-button", function() {    
   player1.diceRoll();
@@ -53,8 +98,8 @@ $("#roll").on("click", "#roll-button", function() {
     player1.currentPlayersScore.pop();
     $("#player2-current-score").show();
     $("#player1-current-score").hide();
-    alert("You rolled a 1 :(")
     $("#kenblock").show();
+    alert("You rolled a 1 :(")
     $('#ryu').hide();
     $('#ken').hide();
     $('#bros').hide();
@@ -68,8 +113,8 @@ $("#roll").on("click", "#roll-button", function() {
     player2.currentPlayersScore.pop();
     $("#player1-current-score").show();
     $("#player2-current-score").hide();
-    alert("You rolled a 1 :(")
     $("#ryublock").show();
+    alert("You rolled a 1 :(")
     $('#ryu').hide();
     $('#ken').hide();
     $("#kenblock").hide();
@@ -105,7 +150,7 @@ $("#roll").on("click", "#roll-button", function() {
     $('#bros').hide();
     $("#kenblock").hide();
     $("#ryublock").hide();
-     $("#player1-current-score").show();
+    $("#player1-current-score").show();
 
      player2.turn = false;
      player1.turn = true;
@@ -115,6 +160,7 @@ $("#roll").on("click", "#roll-button", function() {
   
     if (player1.score >= 100) {
       $("#player1-win").show();
+      $("#kenwin").show();
       $('#ryu').hide();
       $('#bros').hide();
       $("#kenblock").hide();
@@ -123,7 +169,7 @@ $("#roll").on("click", "#roll-button", function() {
       $('#ken-defense').hide();
     } else if (player2.score >= 100) {
       $("#player2-win").show();
-      $("#player1-win").show();
+      $("#ryuwin").show();
       $('#ryu').hide();
       $('#bros').hide();
       $("#kenblock").hide();
